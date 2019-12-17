fdec80b4aba26abd666575084f2dff1218e6ee77
luis <luiscarlos_0831@hotmail.com>
2019-12-17 13:06:51 -0500

    EF

---
+++ BCP_MVCEF/Controllers/ClienteController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BCP_MVCEF.Models;
+
+namespace BCP_MVCEF.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using(DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Cliente.ToList());
+            }
+        }
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int id)
+        {
+            using(DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Cliente.Where(x => x.Id == id).FirstOrDefault());
+            }
+            
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        public ActionResult Create(Cliente cliente)
+        {
+            try
+            {
+                using (DbModels dbModel = new DbModels())
+                {
+                    dbModel.Cliente.Add(cliente);
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Cliente.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Cliente cliente)
+        {
+            try
+            {
+                using(DbModels dbModel = new DbModels())
+                {
+                    dbModel.Entry(cliente).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Cliente.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DbModels dbModel = new DbModels())
+                {
+                    Cliente cliente = dbModel.Cliente.Where(x => x.Id == id).FirstOrDefault();
+                    dbModel.Cliente.Remove(cliente);
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BCP_MVCEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BCP_MVCEF.MvcApplication" Language="C#" %>
